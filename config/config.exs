20290e061e11c387f6c59be647638d5c23c9cc7e
James Harton <james@harton.nz>
2022-10-26 09:21:10 +1300

    chore: fix typo in git_ops config.

--- config/config.exs
+++ config/config.exs
@@ -6,6 +6,6 @@ config :git_ops,
   mix_project: Mix.Project.get!(),
   changelog_file: "CHANGELOG.md",
   repository_url: "https://github.com/team-alembic/ash_gen_server",
-  manage_mix_version: true,
+  manage_mix_version?: true,
   manage_readme_version: "README.md",
   version_tag_prefix: "v"
